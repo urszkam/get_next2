8b884e95fd32cea70ed9601c41d1f5082cb7f12b
Ula Kaminska <s28722@pjwstk.edu.pl>
2026-06-27 09:38:48 +0200

    init commit

--- get_next_line.c
+++ get_next_line.c
@@ -1,32 +1,83 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: urkamins <urkamins@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ukaminsk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/22 19:11:48 by urkamins          #+#    #+#             */
-/*   Updated: 2026/06/22 20:05:21 by urkamins         ###   ########.fr       */
+/*   Updated: 2026/06/26 21:08:35 by ukaminsk         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "get_next_line.h"
+
+static int	free_txt(char **ptr, int ret)
+{
+	if (ptr && *ptr)
+		free(*ptr);
+	*ptr = NULL;
+	return (ret);
+}
+
+static int	read_until_endl(int fd, char **txt)
+{
+	char	*buff;
+	char	*temp;
+	int		bytes;
+
+	buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buff)
+		return (-1);
+	while (!*txt || !ft_strchr(*txt, '\n'))
+	{
+		bytes = read(fd, buff, BUFFER_SIZE);
+		if (bytes <= 0 )
+			return (free_txt(&buff, bytes));
+		buff[bytes] = 0;
+		temp = *txt;
+		*txt = ft_strjoin(temp, buff);
+		free(temp);
+		if (!*txt)
+			return (free_txt(&buff, -1));
+	}
+	return (free_txt(&buff, 1));
+}
+
+static char	*extract_line(char **txt)
+{
+	char	*tmp;
+	char	*line;
+	int		pos;
+
+	pos = 0;
+	while ((*txt)[pos] && (*txt)[pos] != '\n')
+		pos++;
+	tmp = *txt;
+	line = ft_substr(*txt, 0, pos + 1);
+	*txt = ft_substr(*txt, pos + 1, ft_strlen(*txt) - (pos + 1));
+	free(tmp);
+	if (!line || !*txt)
+	{
+		free(line);
+		free_txt(txt, 0);
+		return (NULL);
+	}
+	return (line);
+}
 
 char	*get_next_line(int fd)
 {
 	static char	*txt;
-	char		buff[BUFFER_SIZE];
-	int			endl_idx;
-
-	if (fd < 0)
+	int			is_read;
+	
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	while (read(fd, buff, BUFFER_SIZE) > 0 || txt != NULL)
+	is_read = read_until_endl(fd, &txt);
+	if ((!is_read && (!txt || !*txt)) || is_read < 0)
 	{
-		endl_idx = endl_idx(txt);
-		if ( endl_idx >= 0 )
-			return (ft_substr(txt, 0, endl_idx + 1))
-
+		free_txt(&txt, 0);
+		return (NULL);
 	}
-	return (NULL);
+	return (extract_line(&txt));
 }
-
--- get_next_line.h
+++ get_next_line.h
@@ -1,14 +1,19 @@
-#ifndef GET_NEXT_LINE.H
-# define GET_NEXT_LINE.H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 1
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
 
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
 
 char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *s);
 
 #endif
-
